22ae32d53e7a42103e61c5b3d9fc4ef90becfb69
ForgeDom <maxstill2006@gmail.com>
2025-04-29 19:26:22 +0300

    +

--- SystemProgramming4/Program.cs
+++ SystemProgramming4/Program.cs
@@ -2,8 +2,19 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Task task = Task.Run(() => PrintNumbers());
+        Console.WriteLine("Main thread is doing other work...");
+        await task; 
+        Console.WriteLine("Task completed.");
+    }
+    static void PrintNumbers()
+    {
+        for (int i = 1; i <= 50; i++)
+        {
+            Console.WriteLine(i);
+            Thread.Sleep(100);
+        }
     }
 }
