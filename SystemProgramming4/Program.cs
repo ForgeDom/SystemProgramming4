d0efde52186bfd1550fc8054f6ea1750d4039330
ForgeDom <maxstill2006@gmail.com>
2025-05-02 21:07:54 +0300

    +

--- SystemProgramming4/Program.cs
+++ SystemProgramming4/Program.cs
@@ -3,19 +3,26 @@ using System.Threading.Tasks;
 
 class Program
 {
+    static int[] numbers = new int[10000];
+    static int max, min;
+    static double avg;
     static async Task Main(string[] args)
     {
-        Task task = Task.Run(() => PrintNumbers());
-        Console.WriteLine("Main thread is doing other work...");
-        await task; 
-        Console.WriteLine("Task completed.");
-    }
-    static void PrintNumbers()
-    {
-        for (int i = 1; i <= 50; i++)
+        Random random = new Random();
+        for (int i = 0; i < numbers.Length; i++)
         {
-            Console.WriteLine(i);
-            Thread.Sleep(100);
+            numbers[i] = random.Next(-10000, 10000);
         }
+        
+        Task maxTask = Task.Run(() => max = numbers.Max());
+        Task minTask = Task.Run(() => min = numbers.Min());
+        Task avgTask = Task.Run(() => avg = numbers.Average());
+        
+        await Task.WhenAll(maxTask, minTask, avgTask);
+        
+        Console.WriteLine($"Max : {max}");
+        Console.WriteLine($"Min : {min}");
+        Console.WriteLine($"Avg : {avg}");
+        Console.ReadKey();
     }
 }
