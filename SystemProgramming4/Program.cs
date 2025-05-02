42ea5089b0dd5558255124be7153b82c82f8a598
ForgeDom <maxstill2006@gmail.com>
2025-05-02 19:34:41 +0300

    +

--- SystemProgramming4/Program.cs
+++ SystemProgramming4/Program.cs
@@ -3,51 +3,19 @@ using System.Threading.Tasks;
 
 class Program
 {
-    static int[] numbers = new int[10000];
-    static int max, min;
-    static double avg;
-    static readonly string outputPath = "results.txt";
     static async Task Main(string[] args)
     {
-        Random random = new Random();
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            numbers[i] = random.Next(-10000, 10000);
-        }
-        
-        Task maxTask = Task.Run(() => max = numbers.Max());
-        Task minTask = Task.Run(() => min = numbers.Min());
-        Task avgTask = Task.Run(() => avg = numbers.Average());
-        
-        await Task.WhenAll(maxTask, minTask, avgTask);
-        
-        Task fileTask = Task.Run(() => SaveResultsToFile());
-
-        await fileTask;
-        Console.WriteLine($"Max : {max}");
-        Console.WriteLine($"Min : {min}");
-        Console.WriteLine($"Avg : {avg}");
-        Console.WriteLine("Result saved to results.txt");
-        Console.ReadKey();
+        Task task = Task.Run(() => PrintNumbers());
+        Console.WriteLine("Main thread is doing other work...");
+        await task; 
+        Console.WriteLine("Task completed.");
     }
-    
-    static void SaveResultsToFile()
+    static void PrintNumbers()
     {
-        try
+        for (int i = 1; i <= 50; i++)
         {
-            using (var writer = new StreamWriter(outputPath))
-            {
-                writer.WriteLine("Generated numbers:");
-                writer.WriteLine(string.Join(", ", numbers)); 
-
-                writer.WriteLine($"\nMaximum: {max}");
-                writer.WriteLine($"Minimum: {min}");
-                writer.WriteLine($"Average: {avg:F2}");
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error writing file: {ex.Message}");
+            Console.WriteLine(i);
+            Thread.Sleep(100);
         }
     }
 }
